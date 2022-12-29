856031468fb10327ab825df7f079c2b3c2721f19
Cloud Huang <cheng.hao.huang@hp.com>
2022-12-29 17:05:50 +0800

    fix code

--- ptt.py
+++ ptt.py
@@ -2,46 +2,85 @@ import logging
 import sys
 import time
 import PyPtt
+import pyodbc
+import uuid
+from datetime import datetime
 
 ptt_bot = PyPtt.API()
 try:
-    ptt_bot.login('帳號', 'pw', False)
+    ptt_bot.login('account', 'PW', False)
+    # start_index = 19050
+    start_index = 19486
+    last_index = 111104
     newest_index = ptt_bot.get_newest_index(PyPtt.NewIndex.BOARD, 'Gossiping')
 
-    print(newest_index)
+    # print(newest_index)
 
-    # .. login ..
-    # post_info = ptt_bot.get_post('Gossiping', index=newest_index)
-    post_info = ptt_bot.get_post('Gossiping', index=221893)    
+    for i in range(start_index, last_index):
+        # post_info = ptt_bot.get_post('Gossiping', index=newest_index)
+        post_info = ptt_bot.get_post('Gossiping', index=i)
 
-    print(post_info)
-    print('文章編號:', post_info[PyPtt.PostField.index])
-    print('作者:', post_info[PyPtt.PostField.author])
-    print('標題:', post_info[PyPtt.PostField.title])
-    print('日期:', post_info[PyPtt.PostField.date])
-    print('文章網址:', post_info[PyPtt.PostField.url])
-    print('文章 IP:', post_info[PyPtt.PostField.ip])
-    print('文章推文數量:', post_info[PyPtt.PostField.push_number])
-    print('文章推文:', post_info[PyPtt.PostField.comments])
-    print('文章內容:', post_info[PyPtt.PostField.content])
+        # print(post_info)
+        if post_info[PyPtt.PostField.post_status] == PyPtt.PostStatus.EXISTS:
+            total_score = 0
+            score = 0
+            for comment in post_info[PyPtt.PostField.comments]:
+                if comment[PyPtt.CommentField.type] == PyPtt.CommentType.BOO:
+                    score = -1
+                elif comment[PyPtt.CommentField.type] == PyPtt.CommentType.PUSH:
+                    score = 1
+                else:
+                    score = 0
 
-    for comment in post_info[PyPtt.PostField.comments]:
-        print('推文作者:', comment[PyPtt.CommentField.author])
-        print('推文類型:', comment[PyPtt.CommentField.type])
+                total_score += score
 
-    if post_info[PyPtt.PostField.post_status] == PyPtt.PostStatus.EXISTS:
-        print('文章存在！')
-    elif post_info[PyPtt.PostField.post_status] == PyPtt.PostStatus.DELETED_BY_AUTHOR:
-        print('文章被作者刪除')
-        sys.exit()
-    elif post_info[PyPtt.PostField.post_status] == PyPtt.PostStatus.DELETED_BY_MODERATOR:
-        print('文章被版主刪除')
-        sys.exit()
+            # Specifying the ODBC driver, server name, database, etc. directly
+            cnxn_str = ("Driver={SQL Server Native Client 11.0};"
+                        "Server=server;"
+                        "Database=dbname;"
+                        "UID=account;"
+                        "PWD=password;")
+            cnxn = pyodbc.connect(cnxn_str)
+            # Create a cursor from the connection
+            cursor = cnxn.cursor()
 
-    if not post_info[PyPtt.PostField.pass_format_check]:
-        print('未通過格式檢查')
-        sys.exit()
+            if post_info[PyPtt.PostField.date] is None:
+                datetime_str = None
+            else:
+                datetime_str = datetime.strptime(
+                    post_info[PyPtt.PostField.date], '%a %b %d %H:%M:%S %Y')
 
-    print('文章資料可以使用')
+            sql = "INSERT INTO dbo.post(Id,title,author,date,content,ip,score,[Url]) VALUES(?,?,?,?,?,?,?,?)"
+            
+            cursor.execute(sql, post_info[PyPtt.PostField.index], post_info[PyPtt.PostField.title], post_info[PyPtt.PostField.author],
+                           datetime_str, post_info[PyPtt.PostField.content], post_info[PyPtt.PostField.ip], total_score, post_info[PyPtt.PostField.url])
+
+            # print(post_info)
+            print('文章編號:', post_info[PyPtt.PostField.index])
+            # print('作者:', post_info[PyPtt.PostField.author])
+            print('標題:', post_info[PyPtt.PostField.title])
+            # print('日期:', post_info[PyPtt.PostField.date])
+            # print('文章網址:', post_info[PyPtt.PostField.url])
+            # print('文章 IP:', post_info[PyPtt.PostField.ip])
+            # print('文章推文數量:', post_info[PyPtt.PostField.push_number])
+            # print('文章推文:', post_info[PyPtt.PostField.comments])
+            # print('文章內容:', post_info[PyPtt.PostField.content])
+
+            for comment in post_info[PyPtt.PostField.comments]:
+                if comment[PyPtt.CommentField.type] == PyPtt.CommentType.BOO:
+                    score = -1
+                elif comment[PyPtt.CommentField.type] == PyPtt.CommentType.PUSH:
+                    score = 1
+                else:
+                    score = 0
+
+                sql = "INSERT INTO dbo.postcomment(Id,postId,[user],[content],score,ip,commenttime) VALUES(?,?,?,?,?,?,?)"
+                cursor.execute(
+                    sql, str(uuid.uuid1()), post_info[PyPtt.PostField.index], comment[PyPtt.CommentField.author], comment[PyPtt.CommentField.content], score, comment[PyPtt.CommentField.ip], comment[PyPtt.CommentField.time])
+
+        cnxn.commit()
+        print('文章資料可以使用')
 finally:
     ptt_bot.logout()
+    cnxn.commit()
+    cnxn.close()
